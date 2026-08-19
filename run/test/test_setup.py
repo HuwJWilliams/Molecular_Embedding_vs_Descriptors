0bd88efa994263f448cbb8bdb19117463a045a20
Huw <=>
2026-08-19 12:54:59 +0100

    editing test_setup.py test script

--- run/test/test_setup.py
+++ run/test/test_setup.py
@@ -13,24 +13,55 @@ SCRIPTS_DIR = ROOT / "scripts"
 SRC_DIR = SCRIPTS_DIR / "src"
 
 sys.path.insert(0, str(SRC_DIR / "pathing"))
-from get_paths import createPathingJSON, addRawDataPaths, addNewDatasetPaths
+from get_paths import createPathingJSON, addRawDataPaths, addNewDatasetPaths, loadJSON
 
 # %%
 test_json_name = "test.json"
 
-createPathingJSON(json_name=test_json_name)
+expected_json = {
+    "imp_dirs": {
+        "proj_dir": "${PROJ_DIR}",
+        "scripts_dir": "${SCRIPTS_DIR}",
+        "src_dir": "${SRC_DIR}",
+        "datasets_dir": "${DATASETS_DIR}",
+        "results_dir": "${RESULTS_DIR}",
+    },
+    "train_test_splits": {},
+    "raw_data": {"test": str(RUN_DIR / "test" / "dummy_data.csv")},
+    "targets": {"test": "${DATASETS_DIR}/test/dummy_data.csv"},
+    "full_features": {"test": {"rdkit": "${DATASETS_DIR}/test/test_rdkit.csv"}},
+    "prediction_output_dirs": {
+        "rf": {"test": {"rdkit": "${RESULTS_DIR}/TEST_predictions_rf/rdkit"}},
+        "cross_feature_predictions": {"test": {}},
+        "lipinski_cross_feature_predictions": {"test": {}},
+    },
+    "dataset_analysis": {},
+    "config": {},
+}
 
-addRawDataPaths(
-    raw_data_paths=[str(RUN_DIR / "dummy_data.csv")],
-    set_names=["test"],
-    json_name=test_json_name,
-)
 
-addNewDatasetPaths(
-    dataset_key="test",
-    target_file="dummy_data.csv",
-    dataset_prefix="TEST",
-    dataset_folder_name="test",
-    feature_sets=["rdkit"],
-    json_name=test_json_name,
-)
+def testSetup():
+    createPathingJSON(json_name=test_json_name)
+
+    addRawDataPaths(
+        raw_data_paths=[str(RUN_DIR / "test" / "dummy_data.csv")],
+        set_names=["test"],
+        json_name=test_json_name,
+    )
+
+    addNewDatasetPaths(
+        dataset_key="test",
+        target_file="dummy_data.csv",
+        dataset_prefix="TEST",
+        dataset_folder_name="test",
+        feature_sets=["rdkit"],
+        json_name=test_json_name,
+    )
+
+    generated_json = loadJSON(SRC_DIR / "pathing" / test_json_name)
+
+    assert (
+        generated_json == expected_json
+    ), "Generated pathing JSON does not match the expected standard"
+
+    print("Pathing JSON configured")
